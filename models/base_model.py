9bdba40432e924bd2eebb1db180b04c13bda94bc
Geu Aguto Garang <133713642+Geu-Pro2023@users.noreply.github.com>
2024-02-10 15:28:45 +0200

    update

---
+++ models/base_model.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+"""This module defines a base class for all models in our hbnb clone"""
+import uuid
+from datetime import datetime
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, DATETIME, String
+import models
+
+if models.storage_t != "db":
+    Base = object
+else:
+    Base = declarative_base()
+
+
+class BaseModel:
+    """A base class for all hbnb models"""
+    if models.storage_t == "db":
+        id = Column(String(60), primary_key=True, default=str(uuid.uuid4()))
+        created_at = Column(DATETIME, default=datetime.utcnow())
+        updated_at = Column(DATETIME, default=datetime.utcnow())
+
+    def __init__(self, *args, **kwargs):
+        """Instatntiates a new model"""
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            if kwargs.get("updated_at") is None\
+                    or kwargs.get("created_at") is None:
+                self.updated_at = self.created_at = datetime.now()
+            for k, v in kwargs.items():
+                if k == "__class__":
+                    continue
+                if k == "updated_at" or k == "created_at":
+                    v = datetime.fromisoformat(v)
+                setattr(self, k, v)
+
+    def __str__(self):
+        """Returns a string representation of the instance"""
+        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
+        return '[{}] ({}) {}'.format(cls, self.id, self.to_dict())
+
+    def save(self):
+        """Updates updated_at with current time when instance is changed"""
+        from models import storage
+        self.updated_at = datetime.now()
+        storage.new(self)
+        storage.save()
+
+    def to_dict(self):
+        """Convert instance into dict format"""
+        dictionary = {}
+        dictionary.update(self.__dict__)
+        dictionary.update({'__class__':
+                          (str(type(self)).split('.')[-1]).split('\'')[0]})
+        dictionary['created_at'] = self.created_at.isoformat()
+        dictionary['updated_at'] = self.updated_at.isoformat()
+        if dictionary.get("_sa_instance_state") is not None:
+            del dictionary["_sa_instance_state"]
+        return dictionary
+
+    def delete(self):
+        """
+        deleting an obj from storage
+        """
+        models.storage.delete(self)
---
+++ tests/test_models/test_user.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" """
+from tests.test_models.test_base_model import test_basemodel
+from models.user import User
+
+
+class test_User(test_basemodel):
+    """ """
+
+    def __init__(self, *args, **kwargs):
+        """ """
+        super().__init__(*args, **kwargs)
+        self.name = "User"
+        self.value = User
+
+    def test_first_name(self):
+        """ """
+        new = self.value()
+        new.first_name = "Ronald"
+        self.assertEqual(type(new.first_name), str)
+
+    def test_last_name(self):
+        """ """
+        new = self.value()
+        new.last_name = "Abimbola"
+        self.assertEqual(type(new.last_name), str)
+
+    def test_email(self):
+        """ """
+        new = self.value()
+        new.email = "r.abimbola@alustudent.com"
+        self.assertEqual(type(new.email), str)
+
+    def test_password(self):
+        """ """
+        new = self.value()
+        new.password = "ronald123"
+        self.assertEqual(type(new.password), str)
