e6ce72c27f21d6424ce5d7675c5b145690148d83
Geu Aguto Garang <133713642+Geu-Pro2023@users.noreply.github.com>
2024-02-10 14:29:56 +0200

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
+from models import storage_t
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    __tablename__ = 'users'
-
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-    places = relationship("Place", backref="user", cascade="delete")
-    reviews = relationship("Review", backref="user", cascade="delete")
+    if storage_t == "db":
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128))
+        last_name = Column(String(128))
+        places = relationship('Place', backref='user')
+        reviews = relationship('Review', backref='user')
+    else:
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
