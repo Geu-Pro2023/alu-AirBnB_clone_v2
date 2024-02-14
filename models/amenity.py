cd1ed130ddc48c22754fd62ecb5f85662aa37f5f
Geu-Pro2023 <g.bior@alustudent.com>
2024-02-14 12:57:46 -0800

    commit files

--- models/amenity.py
+++ models/amenity.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-<<<<<<< HEAD
 import os
-=======
-from os import getenv
->>>>>>> cdb985bf1439ce5eb65b582b4c93fc576612c422
 
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
@@ -12,15 +8,8 @@ from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-<<<<<<< HEAD
     """Amenities of a place"""
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-=======
-    '''amenity class'''
-    __tablename__ = 'amenities'
-    name = Column(String(128), nullable=False)
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
->>>>>>> cdb985bf1439ce5eb65b582b4c93fc576612c422
         place_amenities = relationship('Place', secondary="place_amenity")
