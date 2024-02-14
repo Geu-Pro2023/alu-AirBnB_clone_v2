41be5a98f964a2b67421f7de88fdb22faf113122
Geu-Pro2023 <g.bior@alustudent.com>
2024-02-14 15:47:54 -0800

    updated

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,29 +1,24 @@
 #!/usr/bin/python3
-"""A module for web application deployment with Fabric."""
-import os
+"""
+    script that generates '.tgz' archive from the contents of the 'web_static'
+"""
+from fabric.api import local
 from datetime import datetime
-from fabric.api import local, runs_once
 
 
-@runs_once
 def do_pack():
-    """Archives the static files."""
-    if not os.path.isdir("versions"):
-        os.mkdir("versions")
-    cur_time = datetime.now()
-    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-        cur_time.year,
-        cur_time.month,
-        cur_time.day,
-        cur_time.hour,
-        cur_time.minute,
-        cur_time.second
-    )
-    try:
-        print("Packing web_static to {}".format(output))
-        local("tar -cvzf {} web_static".format(output))
-        archize_size = os.stat(output).st_size
-        print("web_static packed: {} -> {} Bytes".format(output, archize_size))
-    except Exception:
-        output = None
-    return output
+    """
+        function to compress directory into .tgz archive
+        Return: Success - '.tgz' archive path
+                Failure - None
+    """
+    now = datetime.now()
+    now = now.strftime('%Y%m%d%H%M%S')
+    archive_path = 'versions/web_static_' + now + '.tgz'
+
+    local('mkdir -p versions/')
+    result = local('tar -cvzf {} web_static/'.format(archive_path))
+
+    if result.succeeded:
+        return archive_path
+    return None
