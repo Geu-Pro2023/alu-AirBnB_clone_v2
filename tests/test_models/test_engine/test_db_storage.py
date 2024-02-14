28f2dd2cc730ee0690350933cff1326f2225b4be
Geu-Pro2023 <g.bior@alustudent.com>
2024-02-14 14:15:02 -0800

    github

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 import unittest
 import models
 from models.user import User
@@ -11,179 +10,60 @@ from models.city import City
 import os
 
 
-# skip these tests if db is not the storage
+# skip these test if the storage is not db
 @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db', "skip if not fs")
-class TestDBSorage(unittest.TestCase):
-    """test DB Storage"""
+class TestDBStorage(unittest.TestCase):
+    """DB Storage test"""
 
     def setUp(self):
-        """set up the test environment"""
+        """ Set up test environment """
         self.storage = models.storage
 
     def tearDown(self):
-        """Remove storage file at the end of tests"""
+        """ Remove storage file at end of tests """
         del self.storage
 
     def test_user(self):
-        """Tests the user"""
-        user = User(name="Brian", email="brian@gmail.com", password="Brian123")
+        """ Tests user """
+        user = User(name="Abissa")
         user.save()
-        self.assertFalse(user.id in self.storage.all())
-        self.assertEqual(user.name, "Brian")
+        self.assertTrue(user.id in self.storage.all())
+        self.assertEqual(user.name, "Abissa")
 
     def test_city(self):
-        """ Tests the city """
-        state = State(name="Kenya")
-        state.save()
-        city = City(name="Nairobi")
+        """ test user """
+        city = City(name="Maradi")
+        state = State()
         city.state_id = state.id
         city.save()
-        self.assertFalse(city.id in self.storage.all())
-        self.assertEqual(city.name, "Nairobi")
+        self.assertTrue(city.id in self.storage.all())
+        self.assertEqual(city.name, "Maradi")
 
     def test_state(self):
-        """Test state"""
-        state = State(name="Kenya")
+        """ test state"""
+        state = State(name="California")
         state.save()
-        self.assertFalse(state.id in self.storage.all())
-        self.assertEqual(state.name, "Kenya")
+        self.assertTrue(state.id in self.storage.all())
+        self.assertEqual(state.name, "California")
 
     def test_place(self):
-        """ Test the place"""
-        state = State(name="Kenya")
-        state.save()
-
-        city = City(name="Nairobi")
-        city.state_id = state.id
-        city.save()
-
-        user = User(name="Brian", email="brian@gmail.com", password="Brian123")
-        user.save()
-
-        place = Place(name="PentHouse", number_rooms=4)
-        place.city_id = city.id
-        place.user_id = user.id
+        """ test place """
+        place = Place(name="Palace", number_rooms=4)
         place.save()
-
-        self.assertFalse(place.id in self.storage.all())
+        self.assertTrue(place.id in self.storage.all())
         self.assertEqual(place.number_rooms, 4)
-        self.assertEqual(place.name, "PentHouse")
+        self.assertEqual(place.name, "Palace")
 
     def test_amenity(self):
-        """tests the amenity"""
-        amenity = Amenity(name="Spoon")
+        """ test amenity """
+        amenity = Amenity(name="Startlink")
         amenity.save()
-        self.assertFalse(amenity.id in self.storage.all())
-        self.assertEqual(amenity.name, "Spoon")
+        self.assertTrue(amenity.id in self.storage.all())
+        self.assertTrue(amenity.name, "Startlink")
 
     def test_review(self):
-        """Tests the review"""
-        state = State(name="Kenya")
-        state.save()
-
-        city = City(name="Nairobi")
-        city.state_id = state.id
-        city.save()
-
-        user = User(name="Brian", email="brian@gmail.com", password="Brian123")
-        user.save()
-
-        place = Place(name="PentHouse", number_rooms="4")
-        place.city_id = city.id
-        place.user_id = user.id
-        place.save()
-
-        review = Review(text="work smart", place_id=place.id, user_id=user.id)
+        """ test review """
+        review = Review(text="no comment")
         review.save()
-
-        self.assertFalse(review.id in self.storage.all())
-        self.assertEqual(review.text, "work smart")
-
-
-    if __name__ == '__main__':
-        unittest.main()
-=======
-"""Defines the DBStorage engine."""
-from os import getenv
-from models.base_model import Base
-from models.base_model import BaseModel
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
-from sqlalchemy import create_engine
-from sqlalchemy.orm import relationship
-from sqlalchemy.orm import scoped_session
-from sqlalchemy.orm import sessionmaker
-
-
-class DBStorage:
-    """Represents a database storage engine.
-
-    Attributes:
-        __engine (sqlalchemy.Engine): The working SQLAlchemy engine.
-        __session (sqlalchemy.Session): The working SQLAlchemy session.
-    """
-
-    __engine = None
-    __session = None
-
-    def __init__(self):
-        """Initialize a new DBStorage instance."""
-        self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".
-                                      format(getenv("HBNB_MYSQL_USER"),
-                                             getenv("HBNB_MYSQL_PWD"),
-                                             getenv("HBNB_MYSQL_HOST"),
-                                             getenv("HBNB_MYSQL_DB")),
-                                      pool_pre_ping=True)
-        if getenv("HBNB_ENV") == "test":
-            Base.metadata.drop_all(self.__engine)
-
-    def all(self, cls=None):
-        """Query on the curret database session all objects of the given class.
-
-        If cls is None, queries all types of objects.
-
-        Return:
-            Dict of queried classes in the format <class name>.<obj id> = obj.
-        """
-        if cls is None:
-            objs = self.__session.query(State).all()
-            objs.extend(self.__session.query(City).all())
-            objs.extend(self.__session.query(User).all())
-            objs.extend(self.__session.query(Place).all())
-            objs.extend(self.__session.query(Review).all())
-            objs.extend(self.__session.query(Amenity).all())
-        else:
-            if type(cls) == str:
-                cls = eval(cls)
-            objs = self.__session.query(cls)
-        return {"{}.{}".format(type(o).__name__, o.id): o for o in objs}
-
-    def new(self, obj):
-        """Add obj to the current database session."""
-        self.__session.add(obj)
-
-    def save(self):
-        """Commit all changes to the current database session."""
-        self.__session.commit()
-
-    def delete(self, obj=None):
-        """Delete obj from the current database session."""
-        if obj is not None:
-            self.__session.delete(obj)
-
-    def reload(self):
-        """Create all tables in the database and initialize a new session."""
-        Base.metadata.create_all(self.__engine)
-        session_factory = sessionmaker(bind=self.__engine,
-                                       expire_on_commit=False)
-        Session = scoped_session(session_factory)
-        self.__session = Session()
-
-    def close(self):
-        """Close the working SQLAlchemy session."""
-        self.__session.close()
->>>>>>> cdb985bf1439ce5eb65b582b4c93fc576612c422
+        self.assertTrue(review.id in self.storage.all())
+        self.assertEqual(review.text, "no comment")
--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 """ Module for testing file storage"""
 import unittest
 
@@ -9,7 +8,7 @@ import os
 
 
 @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') == 'db', "skip if not db")
-class test_fileStorage(unittest.TestCase):
+class TestFileStorage(unittest.TestCase):
     """ Class to test the file storage method """
 
     def setUp(self):
@@ -23,69 +22,13 @@ class test_fileStorage(unittest.TestCase):
 
     def tearDown(self):
         """ Remove storage file at end of tests """
-=======
-"""Defines the FileStorage class."""
-import json
-from models.base_model import BaseModel
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
-
-
-class FileStorage:
-    """Represent an abstracted storage engine.
-
-    Attributes:
-        __file_path (str): The name of the file to save objects to.
-        __objects (dict): A dictionary of instantiated objects.
-    """
-
-    __file_path = "file.json"
-    __objects = {}
-
-    def all(self, cls=None):
-        """Return a dictionary of instantiated objects in __objects.
-
-        If a cls is specified, returns a dictionary of objects of that type.
-        Otherwise, returns the __objects dictionary.
-        """
-        if cls is not None:
-            if type(cls) == str:
-                cls = eval(cls)
-            cls_dict = {}
-            for k, v in self.__objects.items():
-                if type(v) == cls:
-                    cls_dict[k] = v
-            return cls_dict
-        return self.__objects
-
-    def new(self, obj):
-        """Set in __objects obj with key <obj_class_name>.id."""
-        self.__objects["{}.{}".format(type(obj).__name__, obj.id)] = obj
-
-    def save(self):
-        """Serialize __objects to the JSON file __file_path."""
-        odict = {o: self.__objects[o].to_dict() for o in self.__objects.keys()}
-        with open(self.__file_path, "w", encoding="utf-8") as f:
-            json.dump(odict, f)
-
-    def reload(self):
-        """Deserialize the JSON file __file_path to __objects, if it exists."""
->>>>>>> cdb985bf1439ce5eb65b582b4c93fc576612c422
         try:
-            with open(self.__file_path, "r", encoding="utf-8") as f:
-                for o in json.load(f).values():
-                    name = o["__class__"]
-                    del o["__class__"]
-                    self.new(eval(name)(**o))
-        except FileNotFoundError:
+            os.remove('file.json')
+        except:
             pass
+
         del self.storage
 
-<<<<<<< HEAD
     def test_obj_list_empty(self):
         """ __objects is initially empty """
         self.assertEqual(len(self.storage.all()), 0)
@@ -162,22 +105,10 @@ class FileStorage:
         _id = new.to_dict()['id']
         for key in self.storage.all().keys():
             temp = key
-            self.assertEqual(temp, 'BaseModel' + ',' + _id)
+            self.assertEqual(temp, 'BaseModel' + '.' + _id)
 
     def test_storage_var_created(self):
         """ FileStorage object storage created """
         from models.engine.file_storage import FileStorage
         print(type(self.storage))
         self.assertEqual(type(self.storage), FileStorage)
-=======
-    def delete(self, obj=None):
-        """Delete a given object from __objects, if it exists."""
-        try:
-            del self.__objects["{}.{}".format(type(obj).__name__, obj.id)]
-        except (AttributeError, KeyError):
-            pass
-
-    def close(self):
-        """Call the reload method."""
-        self.reload()
->>>>>>> cdb985bf1439ce5eb65b582b4c93fc576612c422
