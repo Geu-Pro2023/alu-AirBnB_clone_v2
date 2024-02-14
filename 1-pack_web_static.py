4c27e1b643fca107866ce8dc041ecb38755c0348
Geu-Pro2023 <g.bior@alustudent.com>
2024-02-14 15:04:24 -0800

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,38 +1,29 @@
-import unittest
-import MySQLdb
+#!/usr/bin/python3
+"""A module for web application deployment with Fabric."""
+import os
+from datetime import datetime
+from fabric.api import local, runs_once
 
-class TestDatabaseFunctionality(unittest.TestCase):
-    def setUp(self):
-        # Connect to the test database
-        self.conn = MySQLdb.connect(
-            user='hbnb_test',
-            password='hbnb_test_pwd',
-            host='localhost',
-            database='hbnb_test_db'
-        )
-        self.cursor = self.conn.cursor()
 
-    def tearDown(self):
-        # Close the database connection
-        self.cursor.close()
-        self.conn.close()
-
-    def test_create_state(self):
-        # Get the initial number of records in the states table
-        self.cursor.execute("SELECT COUNT(*) FROM states")
-        initial_count = self.cursor.fetchone()[0]
-
-        # Execute the action (e.g., create a new state)
-        # Assume that you have a function to create a state in your application
-        create_state_function()  # Replace with the actual function call
-
-        # Get the number of records in the states table again
-        self.cursor.execute("SELECT COUNT(*) FROM states")
-        final_count = self.cursor.fetchone()[0]
-
-        # Validate the action
-        self.assertEqual(final_count, initial_count + 1, "New state was not created")
-
-if __name__ == '__main__':
-    unittest.main()
-
+@runs_once
+def do_pack():
+    """Archives the static files."""
+    if not os.path.isdir("versions"):
+        os.mkdir("versions")
+    cur_time = datetime.now()
+    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        cur_time.year,
+        cur_time.month,
+        cur_time.day,
+        cur_time.hour,
+        cur_time.minute,
+        cur_time.second
+    )
+    try:
+        print("Packing web_static to {}".format(output))
+        local("tar -cvzf {} web_static".format(output))
+        archize_size = os.stat(output).st_size
+        print("web_static packed: {} -> {} Bytes".format(output, archize_size))
+    except Exception:
+        output = None
+    return output
